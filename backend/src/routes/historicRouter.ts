83f0bb1ed43431503145c29b57ad681678c51844
Valdir Silva <valdir.ti@gmail.com>
2025-03-29 13:01:13 -0300

    feat: backend delete historic

--- backend/src/routes/historicRouter.ts
+++ backend/src/routes/historicRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { historic } from "../controllers/historicController";
+import { historic, deleteHistoric } from "../controllers/historicController";
 
 const historicCheckRouter = Router();
 
 historicCheckRouter.get("/historico", historic);
+historicCheckRouter.delete("/historico/:id", deleteHistoric);
 
 export default historicCheckRouter;
