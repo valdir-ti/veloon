6d4e27820c4b3ca1c03c724231a56a55ef98230c
Valdir Silva <valdir.ti@gmail.com>
2025-03-30 21:11:34 -0300

    refact: some lint changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    port: 3000,
-    host: true,
-  },
-  build: {
-    outDir: "build",
-  },
-});
+	plugins: [react(), tailwindcss()],
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		port: 3000,
+		host: true,
+	},
+	build: {
+		outDir: 'build',
+	},
+})
